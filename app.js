40361b881acb62b22f992928c1a65ae6b2e0f501
Reshali195 <65700981+Reshali195@users.noreply.github.com>
2022-06-19 23:10:59 +0530

    first

---
+++ app.js
@@ -0,0 +1,17 @@
+//import "./public/index.css";
+class Quote extends React.Component {
+    render() {
+        return (
+          <div className=" hello" >
+              <div className="item-content watch-box">
+                <div ><img class="item-img" src="https://media.istockphoto.com/photos/close-up-of-black-wrist-watch-with-golden-details-picture-id1386061196?b=1&k=20&m=1386061196&s=170667a&w=0&h=O5rgW7KPqLAjrz05XLASmW43ZqCRPdX8ow6SwM8DfD8=" /></div>
+                <div>price=$667686</div>
+                <div><button>next</button></div>
+              </div>
+
+              
+            </div>
+            )
+          }
+}
+ReactDOM.render(<Quote/>,document.getElementById("pool"));
